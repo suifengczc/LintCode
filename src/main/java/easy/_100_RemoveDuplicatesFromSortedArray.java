8dc12dfd3856e8b1bf090c45c7f864610ff25554
suifengczc <ssuifengczc@163.com>
2018-02-22 23:27:18 +0800

    2018-2-22 23:28

--- src/main/java/easy/_100_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/easy/_100_RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,7 @@
 package easy;
 
+import com.sun.org.apache.xpath.internal.WhitespaceStrippingElementMatcher;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -9,6 +11,7 @@ public class _100_RemoveDuplicatesFromSortedArray {
     /*
      * @param nums: An ineger array
      * @return: An integer
+     * solution_1 使用了额外的map，不符合题意
      */
     public int removeDuplicates(int[] nums) {
         // write your code here
@@ -29,5 +32,33 @@ public class _100_RemoveDuplicatesFromSortedArray {
             nums[index++] = key;
         }
         return map.size();
+    }
+
+    /**
+     * @param nums: An ineger array
+     * @return: An integer
+     * solution_1 不使用额外的开销，在当前数组中进行转换
+     */
+    public int solution_2(int[] nums) {
+        // write your code here
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int currIndex = 0;
+        int moveIndex = 1;
+        while (moveIndex < nums.length) {
+            while (moveIndex < nums.length && nums[currIndex] == nums[moveIndex]) {
+                moveIndex++;
+            }
+            if (moveIndex < nums.length && nums[currIndex] != nums[moveIndex]) {
+                nums[currIndex + 1] = nums[moveIndex];
+                currIndex++;
+                moveIndex++;
+            }
+        }
+        return currIndex + 1;
     }
 }
---
+++ src/main/java/easy/_101_RemoveDuplicatesFromSortedArrayii.java
@@ -0,0 +1,42 @@
+package easy;
+
+public class _101_RemoveDuplicatesFromSortedArrayii {
+    /**
+     * @param nums: a array of integers
+     * @return : return an integer
+     * 允许重复两次
+     */
+    public static int removeDuplicates(int[] nums) {
+        // write your code here
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+        int currIndex = 0;
+        int sameCount = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[currIndex]) {
+                if (sameCount < 2) {
+                    nums[++currIndex] = nums[i];
+                    sameCount++;
+                }
+            } else {
+                nums[++currIndex] = nums[i];
+                sameCount = 1;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print("\"" + nums[i] + "\", ");
+        }
+        return currIndex + 1;
+    }
+
+    public static void main(String[] args) {
+//        int[] a = {-1, -1, -1, 0, 1, 2, 2, 2, 2, 3, 4, 5};
+//        int[] a = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1};
+        int[] a = {-8, 0, 1, 2, 3};
+        System.out.println("\n" + removeDuplicates(a));
+    }
+}
